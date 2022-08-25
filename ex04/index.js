591ae22bd3028acb628671a3694b811ef659ea7f
marcio.bueno@unicap.br <marcio-bueno@unicap.br>
2022-08-25 20:22:08 -0300

    limpar campo e setar o focus

--- ex04/index.js
+++ ex04/index.js
@@ -3,13 +3,14 @@ const btOk = document.getElementById("btOk");
 const h3Nome = document.getElementById("h3Nome");
 
 const handleClickBtOk = () => {
-  const nome = inputNome.value;
+  const nome = inputNome.value.trim();
   if (nome) {
-    // nome != null && nome != ""
     h3Nome.innerHTML = `Nome Digitado = ${inputNome.value}`;
   } else {
     h3Nome.innerHTML = "";
   }
+  inputNome.value = "";
+  inputNome.focus();
 };
 
 btOk.onclick = handleClickBtOk;
