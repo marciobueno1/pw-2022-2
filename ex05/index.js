c4e27e3165fc8df54dd0aa08504c50687c1b9360
marcio.bueno@unicap.br <marcio-bueno@unicap.br>
2022-08-25 20:57:45 -0300

    Adicionado exemplo 05 - lista não ordenada

---
+++ ex05/index.js
@@ -0,0 +1,15 @@
+const inputItem = document.getElementById("inputItem");
+const btOk = document.getElementById("btOk");
+const lista = document.getElementById("lista");
+
+const handleClickBtOk = () => {
+  const item = inputItem.value.trim();
+  if (item) {
+    lista.innerHTML += `<li>${item}</li>`;
+    lista.innerHTML += "<li>" + item + "</li>";
+  }
+  inputItem.value = "";
+  inputItem.focus();
+};
+
+btOk.onclick = handleClickBtOk;
