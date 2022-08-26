fb7bfb984353ea8f49d0d32a7cc5423acf8ec81b
marcio.bueno@unicap.br <marcio-bueno@unicap.br>
2022-08-25 21:41:19 -0300

    Lista não numerada com FOR

---
+++ ex07/index.js
@@ -0,0 +1,20 @@
+const inputNumero = document.getElementById("inputNumero");
+const btOk = document.getElementById("btOk");
+const lista = document.getElementById("lista");
+
+const handleClickBtOk = () => {
+  const numero = parseInt(inputNumero.value.trim());
+  if (!isNaN(numero)) {
+    lista.innerHTML = "";
+    for (let i = 0; i < 10; ++i) {
+      const itemText = document.createTextNode(numero + i);
+      const li = document.createElement("li");
+      li.appendChild(itemText);
+      lista.appendChild(li);
+    }
+  }
+  inputNumero.value = "";
+  inputNumero.focus();
+};
+
+btOk.onclick = handleClickBtOk;
